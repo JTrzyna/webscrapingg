bbd79ee7cc267f4ae7138b15631ac8263509d8d4
JTrzyna <jar.trzyna@gmail.com>
2017-06-26 19:51:46 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Net;
+using System.IO;
+
+namespace WebScrapingg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string url = urlTextBox.Text;
+            string sourceCode = TekstStrony.GetSourceCode(url);
+            StreamWriter sw = new StreamWriter("website.txt");
+            sw.Write(sourceCode);
+            sw.Close();
+        }
+    }
+
+}
